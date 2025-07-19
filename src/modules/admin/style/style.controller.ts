648f85fa4ec223a75cd6630accd8aef26f837a28
Jay Prakash Jain <jayprakash005@gmail.com>
2025-07-20 00:21:26 +0530

    style

--- src/modules/admin/style/style.controller.ts
+++ src/modules/admin/style/style.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller,ParseIntPipe, Get, Req, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { StyleService } from './style.service';
 import { CreateStyleDto } from './dto/create-style.dto';
 import { UpdateStyleDto } from './dto/update-style.dto';
@@ -24,11 +24,15 @@ export class StyleController {
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStyleDto: UpdateStyleDto, @Req() req) {
-    return this.styleService.update(+id, updateStyleDto);
+    return this.styleService.update(+id, updateStyleDto,req.user);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.styleService.remove(+id);
   }
+   @Patch(':id/toggle-status')
+      async toggleStatus(@Param('id', ParseIntPipe) id: number, @Req() req) {
+        return this.styleService.toggleStatus(id, req.user);
+      }
 }
--- src/modules/admin/style/style.service.ts
+++ src/modules/admin/style/style.service.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateStyleDto } from './dto/create-style.dto';
 import { UpdateStyleDto } from './dto/update-style.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Style } from '../../../shared/entities/style.entity';
 import { Repository } from 'typeorm';
+import e from 'express';
 
 @Injectable()
 export class StyleService {
   constructor(
     @InjectRepository(Style)
     private styleRepository: Repository<Style>,
-  ) {}
+  ) { }
 
-  async create(createStyleDto: CreateStyleDto,user: any): Promise<Style> {
+  async create(createStyleDto: CreateStyleDto, user: any): Promise<Style> {
 
-      const style = this.styleRepository.create({
+    const style = this.styleRepository.create({
       ...createStyleDto,
       // createdBy: user.username, // or user.sub (ID), depending on your use case
       createdBy: user.sub.toString(), //userid
     });
     return this.styleRepository.save(style);
- 
+
   }
 
   async findAll(): Promise<Style[]> {
@@ -28,15 +29,38 @@ export class StyleService {
     return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} style`;
-  }
+  async findOne(id: number): Promise<Style> {
 
-  update(id: number, updateStyleDto: UpdateStyleDto) {
-    return `This action updates a #${id} style`;
+    const style = await this.styleRepository.findOne({ where: { id } });
+    if (!style) throw new NotFoundException(`style ${id} not found`);
+    return style;
+
+  }
+    
+  
+
+ async update(id: number, updateStyleDto: UpdateStyleDto, user: any): Promise<Style> {
+     const style = await this.findOne(id);
+      
+    if (!style) throw new NotFoundException(`style ${id} not found`);
+     Object.assign(style, updateStyleDto);
+     style.updatedBy = user.sub.toString()
+    return this.styleRepository.save(style);
+
   }
 
   remove(id: number) {
-    return `This action removes a #${id} style`;
+     const style = await this.findOne(id);
+    return this.styleRepository.remove(style);
   }
+
+  async toggleStatus(id: number, user: any): Promise<Subject> {
+      const style = await this.styleRepository.findOne({ where: { id } });
+      if (!style) throw new NotFoundException(`style ${id} not found`);
+      style.status = !style.status;
+      style.updatedBy = user.sub.toString(); // or user.sub.toString()
+  
+      return this.styleRepository.save(style);
+    }
+  
 }
