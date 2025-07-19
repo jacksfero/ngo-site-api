4ac57ce9184c631319a00ba3a11b8042b73dfc30
Jay Prakash Jain <jayprakash005@gmail.com>
2025-07-19 21:09:40 +0530

    sa

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  
+
+  constructor(private readonly configService: ConfigService) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+if (!jwtSecret) {
+  throw new Error('JWT_SECRET environment variable is not set');
+}
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'jwtSecretKey', // use env var!
+      secretOrKey: jwtSecret,  
     });
   }
+
+  
 
   /* async validate(payload: any) {
     return { userId: payload.sub, username: payload.username, roles: payload.roles };
