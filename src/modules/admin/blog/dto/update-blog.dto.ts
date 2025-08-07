cb34a71101d35d09da4024bddcf6484ac755ce38
Jay Prakash Jain <jayprakash005@gmail.com>
2025-08-07 22:08:21 +0530

    bg

--- src/modules/admin/blog/dto/update-blog.dto.ts
+++ src/modules/admin/blog/dto/update-blog.dto.ts
@@ -1,11 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBlogDto } from './create-blog.dto';
-import { IsOptional,IsString } from 'class-validator';
+import { IsOptional, IsString, IsNumber, IsArray } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class UpdateBlogDto extends PartialType(CreateBlogDto) {
+  @IsOptional()
+  @IsString()
+  titleImage?: string;
 
-   @IsOptional()
-@IsString()
-titleImage?: string;
+  @IsOptional()
+  @IsNumber()
+  categoryId?: number;
 
+  @IsOptional()
+  @IsArray()
+  @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
+  tagIds?: number[];
 }
