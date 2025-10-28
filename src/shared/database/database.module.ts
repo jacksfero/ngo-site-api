65a7d280f9941db5cb12ba6ff0ddfb7ad06a66ea
Jay Prakash Jain <jayprakash005@gmail.com>
2025-10-28 11:57:35 +0530

    as

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -19,7 +19,7 @@ import { DatabaseHealthService } from './database.health.service';
           username: configService.get<string>('DB_USERNAME'),
           password: configService.get<string>('DB_PASSWORD'),
           database: configService.get<string>('DB_NAME'),
-          synchronize: true,
+          synchronize: false,
           autoLoadEntities: true,
           entityPrefix: 'my_',
             // ✅ Valid connection timeouts
